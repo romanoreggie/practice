9a43a488c277048ca7f01d4c0c9355a7fcb44e72
Reggie Romano <romanoreggie@gmail.com>
2018-07-22 11:00:26 -0500

    practicing with VueJs Basics

---
+++ intro/main.js
@@ -0,0 +1,13 @@
+console.log("testtest");
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    title: 'Hello World'
+  },
+  methods: {
+    changeTitle: function(event) {
+      this.title = event.target.value;
+    },
+  }
+});
